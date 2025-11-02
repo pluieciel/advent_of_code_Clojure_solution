df6a27000f59773e77dcd4b41c9650fce7b0064d
Yue ZHAO <yueezhao@amazon.lu>
2025-11-02 06:35:08 +0000

    2019day12

--- 2019/day12.clj
+++ 2019/day12.clj
@@ -20,8 +20,8 @@
   (let [axis (apply map vector pos)
         dv (for [xyz pos]
              (mapv get-dx xyz axis))
-        new-vel (map #(mapv + %1 %2) vel dv)]
-    [(map #(mapv + %1 %2) pos new-vel)
+        new-vel (mapv #(mapv + %1 %2) vel dv)]
+    [(mapv #(mapv + %1 %2) pos new-vel)
      new-vel]))
 
 ;; part1
@@ -30,3 +30,37 @@
      (map (fn [ps] (map #(apply + (map Math/abs %)) ps)))
      (apply map *)
      (apply +))
+
+;; part2
+(def init-state [init-pos init-vel])
+
+(defn gcd [a b]
+  (if (zero? b)
+    a
+    (recur b (mod a b))))
+
+(defn lcm [a b]
+  (/ (* a b) (gcd a b)))
+
+(loop [state init-state
+       cnt 0
+       dict {}]
+  (let [new-state (step state)
+        new-cnt (inc cnt)
+        new-dict (-> dict
+                     (#(let [dict %]
+                         (if (or (dict :x) (not= (map first (apply concat init-state)) (map first (apply concat new-state))))
+                           dict
+                           (assoc dict :x new-cnt))))
+                     (#(let [dict %]
+                         (if (or (dict :y) (not= (map second (apply concat init-state)) (map second (apply concat new-state))))
+                           dict
+                           (assoc dict :y new-cnt))))
+                     (#(let [dict %]
+                         (if (or (dict :z) (not= (map last (apply concat init-state)) (map last (apply concat new-state))))
+                           dict
+                           (assoc dict :z new-cnt)))))]
+    (if (< (count new-dict) 3)
+      (recur new-state new-cnt new-dict)
+      (->> (vals new-dict)
+           (reduce lcm)))))
