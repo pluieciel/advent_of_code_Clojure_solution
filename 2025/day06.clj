3f7f9dcda2611ce8d059c93504b93ed41ca24cea
pluieciel <yorke5@qq.com>
2025-12-06 11:42:06 +0000

    2025day06

--- 2025/day06.clj
+++ 2025/day06.clj
@@ -2,27 +2,25 @@
 (ns day06
   (:require [clojure.string :as str]))
 
-(def data (->> (slurp "2025/in06")
-               str/split-lines
+(def rawdata (->> (slurp "2025/in06")
+                  str/split-lines))
+(def data (->> rawdata
                (map #(map read-string (re-seq #"\d+|\*|\+" %)))))
 (def nums (butlast data))
-(def ops (->> (last data)
-              (map resolve)))
+(def ops (last data))
 
 ;; part 1
 (->> (apply map list nums)
-     (map #(apply %1 %2) ops)
+     (map #(apply (resolve %1) %2) ops)
      (reduce +))
 
 ;; part 2
-(def rawdata (->> (slurp "2025/in06")
-                  str/split-lines
-                  butlast))
 (defn spaces? [s] (every? #(= \space %) s))
 
 (->> rawdata
+     butlast
      (apply map str)
      (partition-by spaces?)
      (remove #(spaces? (first %)))
-     (map #(apply %1 (map read-string %2)) ops)
+     (map #(apply (resolve %1) (map read-string %2)) ops)
      (reduce +))
